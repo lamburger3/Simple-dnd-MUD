74de8d48ccc4ba1ce4606f41eeb436dbd742b5a3
lamburger3 <lamburger3@gmail.com>
2017-12-01 11:33:24 -0800

    Revamp with correct stat gen
    Complete revamp with Grandmaster Matt's assistance, now featuring correct god damned stat rolls.

--- simple dnd mud.py
+++ simple dnd mud.py
@@ -6,24 +6,36 @@ def player_name_input():
     print("Then your legend will be the legend of " + name + ", Level 1 Fighter!")
 player_name_input()
 
-def player_stat_generation():
-    sum_of_dice_rolls = 0
-   
-#Stats are then calculated with a for loop, rolling 4 six-sided die each stat
-#For loop will not randomize and sum up four times each, which was the main goal
-#Instead, it will by calculated by a single roll of a 20 sided die for now
-    for x in xrange(0,4):
-        STR = sum_of_dice_rolls + random.randint(1, 20)
-        DEX = sum_of_dice_rolls + random.randint(1, 20)
-        CON = sum_of_dice_rolls + random.randint(1, 20)
-        INT = sum_of_dice_rolls + random.randint(1, 20)
-        WIS = sum_of_dice_rolls + random.randint(1, 20)
-        CHA = sum_of_dice_rolls + random.randint(1, 20)
-#Results will then be printed
+    
+def roll_stat():
+    die_rolls = []
+    for x in range(0, 4):
+    #Roll 4 dice, and append them to a list
+        die_rolls.append(random.randint(1,6))
+
+   #Sort the values
+    die_rolls = sorted(die_rolls)
+
+    #This is sorted in ascending order, so if we want to drop the lowest value, we ignore the first value
+    die_rolls_drop_lowest = die_rolls[1:]
+
+    #Return the sum
+    return sum(die_rolls_drop_lowest)
+
+def set_stats():
+    STR = roll_stat()
+    DEX = roll_stat()
+    CON = roll_stat()
+    INT = roll_stat()
+    WIS = roll_stat()
+    CHA = roll_stat()
     print("Your STR is " + str(STR))
     print("Your DEX is " + str(DEX))
     print("Your CON is " + str(CON)) 
     print("Your INT is " + str(INT))
     print("Your WIS is " + str(WIS))
     print("Your CHA is " + str(CHA))
-player_stat_generation()
+set_stats()
+
+
+
